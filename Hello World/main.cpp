c641eefcefc2de3ec221d767c0d6a88fe0d407a4
ChrisCampbell559 <141672767+ChrisCampbell559@users.noreply.github.com>
2023-08-07 11:50:06 -0700

    Add files via upload

---
+++ Hello World/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\n\n\n Hello world! \n\n" << endl;
+    cout << "\n\n Hello world from the first day of school \n\n" << endl;
+    return 0;
+}
